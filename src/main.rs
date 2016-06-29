72096011dd618f74e4dea9a385814a8736b70312
Garrett Squire <garrettsquire@gmail.com>
2016-06-29 10:13:26 -0700

    use connection pooling instead of a static mutex

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-#[macro_use] extern crate lazy_static;
-
 extern crate chrono;
 extern crate iron;
+extern crate persistent;
+extern crate r2d2;
+extern crate r2d2_sqlite;
 extern crate router;
 extern crate rusqlite;
 extern crate sha2;
@@ -13,6 +14,13 @@ use iron::prelude::*;
 use iron::Url;
 use iron::modifiers::Redirect;
 use iron::status::Status;
+use iron::typemap::Key;
+
+use persistent::Read;
+
+use r2d2::Pool;
+
+use r2d2_sqlite::SqliteConnectionManager;
 
 use router::Router;
 
@@ -24,16 +32,14 @@ use sha2::sha2::Sha256;
 use url::form_urlencoded;
 
 use std::env;
-use std::sync::Mutex;
 
 const HOST: &'static str = "https://yaus.pw/";
 
-lazy_static! {
-    static ref DB_CONN: Mutex<Connection> = {
-        let db = env::var("SHORT_DB").unwrap();
-        let c = Connection::open(db).unwrap();
-        Mutex::new(c)
-    };
+pub type SqlitePool = Pool<SqliteConnectionManager>;
+
+pub struct YausDb;
+impl Key for YausDb {
+    type Value = SqlitePool;
 }
 
 // A simple macro to return early if the URL can't parse.
@@ -48,12 +54,11 @@ macro_rules! try_url {
     }
 }
 
-fn create_shortened_url(long_url: &str) -> IronResult<Response> {
+fn create_shortened_url(db: &Connection, long_url: &str) -> IronResult<Response> {
     let mut hash = Sha256::new();
     hash.input_str(long_url);
     let locator = hash.result_str();
 
-    let db = DB_CONN.lock().unwrap();
     let timestamp = Local::now().to_rfc3339();
     db.execute("INSERT INTO urls VALUES (NULL, $1, $2, $3)",
         &[&timestamp, &long_url, &&locator[0..7]]).unwrap();
@@ -66,31 +71,28 @@ fn create_shortened_url(long_url: &str) -> IronResult<Response> {
 ///
 /// A 200 means that a shortened URL already exists and has been returned. A 201
 /// response means that a new shortened URL has been created.
-fn check_or_shorten_url(long_url: &str) -> IronResult<Response> {
-    // The scoping here is needed because the lock will only be released once it drops
-    // out of scope.
-    {
-        let db = DB_CONN.lock().unwrap();
-        let mut stmt = db.prepare("SELECT locator FROM urls WHERE url = (?)").unwrap();
-        let mut row = stmt.query_map::<String, _>(&[&long_url], |r| r.get(0)).unwrap();
+fn check_or_shorten_url(db: &Connection, long_url: &str) -> IronResult<Response> {
+    let mut stmt = db.prepare("SELECT locator FROM urls WHERE url = (?)").unwrap();
+    let mut row = stmt.query_map::<String, _>(&[&long_url], |r| r.get(0)).unwrap();
 
-        if let Some(l) = row.next() {
-            return Ok(Response::with((Status::Ok, [HOST, &l.unwrap()].join(""))));
-        }
+    if let Some(l) = row.next() {
+        return Ok(Response::with((Status::Ok, [HOST, &l.unwrap()].join(""))));
     }
-    create_shortened_url(long_url)
+    create_shortened_url(db, long_url)
 }
 
 /// The handler to shorten a URL.
 fn shorten_handler(req: &mut Request) -> IronResult<Response> {
-    match req.url.query {
+    match req.url.query.clone() {
         None => { Ok(Response::with((Status::BadRequest, "URL missing in query"))) },
         Some(ref s) => {
             let mut query_string = form_urlencoded::parse(s.as_bytes());
 
             if let Some(long_url) = query_string.next() {
                 try_url!(&*long_url.1);
-                check_or_shorten_url(&*long_url.1)
+                let pool = req.get::<Read<YausDb>>().unwrap().clone();
+                let db = pool.get().unwrap();
+                check_or_shorten_url(&db, &*long_url.1)
             } else {
                 Ok(Response::with((Status::BadRequest, "Malformed query string")))
             }
@@ -100,8 +102,9 @@ fn shorten_handler(req: &mut Request) -> IronResult<Response> {
 
 /// The handler that redirects to the long URL.
 fn redirect_handler(req: &mut Request) -> IronResult<Response> {
-    let locator = req.extensions.get::<Router>().unwrap().find("locator").unwrap();
-    let db = DB_CONN.lock().unwrap();
+    let locator = req.url.path[0].clone();
+    let pool = req.get::<Read<YausDb>>().unwrap().clone();
+    let db = pool.get().unwrap();
     let mut stmt = db.prepare("SELECT url FROM urls WHERE locator = (?)").unwrap();
     let mut row = stmt.query_map::<String, _>(&[&locator], |r| r.get(0)).unwrap();
 
@@ -123,5 +126,13 @@ fn main() {
     router.get("/:locator", redirect_handler);
     router.get("/", index_handler);
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    let config = r2d2::Config::default();
+    let db = env::var("SHORT_DB").unwrap();
+    let manager = SqliteConnectionManager::new(&db);
+    let pool = r2d2::Pool::new(config, manager).unwrap();
+
+    let mut chain = Chain::new(router);
+    chain.link_before(Read::<YausDb>::one(pool));
+
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
